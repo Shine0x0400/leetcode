f5d864ae0493d8b5d17ab448727dbbe20880f433
zjl <zjl.chn@qq.com>
2019-08-08 20:39:50 +0800

    solve in order

---
+++ app/src/test/java/com/zjl/leetcode/Leet30_SubstringWithConcatenationOfAllWords.java
@@ -0,0 +1,77 @@
+package com.zjl.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ * @author zjl
+ * @date 2019-08-08
+ */
+public class Leet30_SubstringWithConcatenationOfAllWords {
+
+    public List<Integer> findSubstring(String s, String[] words) {
+
+        List<Integer> ans = new ArrayList<>();
+
+        if (words == null || words.length == 0 || s == null || s.length() < words[0].length()) {
+            return ans;
+        }
+
+        Map<String, Integer> map = new HashMap<>();
+        for (String w : words) {
+            map.put(w, map.getOrDefault(w, 0) + 1);
+        }
+
+        int length = words[0].length();
+        for (int i = 0; i < length; i++) {
+            int start = i, end = start;
+            Queue<String> queue = new LinkedList<>();
+
+            while (start + length <= s.length()) {
+
+                // increase window
+                while (end + length <= s.length()) {
+                    String sub = s.substring(end, end + length);
+                    Integer remain = map.get(sub);
+
+                    if (remain != null && remain > 0) {
+                        queue.offer(sub);
+                        map.put(sub, remain - 1);
+                        end += length;
+                    } else {
+                        break;
+                    }
+                }
+
+                // check window
+                if (queue.size() == words.length) {
+                    ans.add(start);
+                }
+
+                // decrease window
+                boolean match = false;
+                while (queue.size() > 0) {
+                    String poll = queue.poll();
+                    start += length;
+                    map.put(poll, map.get(poll) + 1);
+
+                    if (end + length <= s.length() && poll.equals(s.substring(end, end + length))) {
+                        match = true;
+                        break;
+                    }
+                }
+
+                if (!match) {
+                    start = end = end + length;
+                }
+            }
+
+        }
+
+        return ans;
+    }
+}
